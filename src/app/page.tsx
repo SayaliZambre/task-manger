d1031b482adf633d81194f2a46cd0d62672f1474
name <yourgithub@email.com>
2025-02-04 00:15:55 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import AddTodo from "@/components/AddTodo";
 import Navbar from "@/components/Navbar";
 import Todo from "@/components/Todo";
 
-export default function Home() {
+export default function Page() {
   return (
     <main className="flex flex-col items-center min-h-screen px-4 py-10">
       <h1 className="text-3xl font-bold text-white mb-8">
